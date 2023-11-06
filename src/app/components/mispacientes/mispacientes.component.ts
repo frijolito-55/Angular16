a081836f245d328b349da0525d944f7704ebe4e3
jose gomez <js1512927@gmail.com>
2023-11-05 18:02:41 -0600

    tercer proyecto en github

---
+++ src/app/components/mispacientes/mispacientes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mispacientes',
+  templateUrl: './mispacientes.component.html',
+  styleUrls: ['./mispacientes.component.css']
+})
+export class MispacientesComponent {
+
+}
---
+++ src/app/components/subirreceta/subirreceta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subirreceta',
+  templateUrl: './subirreceta.component.html',
+  styleUrls: ['./subirreceta.component.css']
+})
+export class SubirrecetaComponent {
+
+}
